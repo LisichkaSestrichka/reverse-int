bf56637a6cd10824dc9703ec9d2fb62b919ada16
LisichkaSestrichka <79520443+LisichkaSestrichka@users.noreply.github.com>
2021-03-14 21:28:34 +0300

    init: added reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  if (0 > n){
+  n = -n;
+  let unRev = String(n);
+  let a = unRev.split('').reverse().join('');
+  return a;
+ }
+  let unRev = String(n);
+  let a = unRev.split('').reverse().join('');
+  return a;
 }
